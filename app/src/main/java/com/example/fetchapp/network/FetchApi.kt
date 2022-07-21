7545cb1b305e0e1ddd4331e2aba07255d37bbca7
lakeishamccree <lmccree215@icloud.com>
2022-07-21 17:48:25 -0400

    added models and network call

---
+++ app/src/main/java/com/example/fetchapp/network/FetchApi.kt
@@ -0,0 +1,25 @@
+package com.example.fetchapp.network
+
+import com.example.fetchapp.model.FetchListItem
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface FetchApi {
+
+    @GET("hiring.json")
+    fun getList():retrofit2.Call<List<FetchListItem>>
+
+    companion object {
+        var baseUrl = "https://fetch-hiring.s3.amazonaws.com/"
+
+        fun create() : FetchApi {
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(baseUrl)
+                .build()
+
+            return retrofit.create(FetchApi::class.java)
+        }
+    }
+}
